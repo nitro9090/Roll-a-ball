9de5f371445beb75948f2c34101d0d8a51da3a8e
Michael Lindon <nitro9090@yahoo.com>
2015-06-08 15:21:14 -0500

    Roll a ball game
    Finished the first tutorial using the Unity engine for building a roll a
    ball game. Everything works and in the builds folder is a .exe for
    playing the game on windows.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	
+	public float speed = 1;
+	public Text collectText;
+	public Text collectedAll;
+
+	private int collected;
+	
+	private Rigidbody rb;
+	
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+		setCollectText ();
+		collectedAll.text = "";
+	}
+	
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			other.gameObject.SetActive(false);
+			collected++;
+			setCollectText ();
+		}
+	}
+
+	void setCollectText () {
+		collectText.text = "Count " + collected.ToString ();
+		if (collected >= 11) {
+			collectedAll.text = "You Win!";
+		}
+	}
+}
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+	public Vector3 rotationRate;
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (rotationRate * Time.deltaTime);
+	}
+}
